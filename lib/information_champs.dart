a2163ceb1213e20bfc427d77bdf1d0026e50e12d
Yakup LACIN <lightsuksesyon@gmail.com>
2023-03-17 18:47:45 +0300

    text

--- lib/information_champs.dart
+++ lib/information_champs.dart
@@ -14,93 +14,103 @@ class InformationChamps extends StatefulWidget {
 class _InformationChampsState extends State<InformationChamps> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        //automaticallyImplyLeading: false,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      backgroundColor: Colors.black87,
-      extendBodyBehindAppBar: true,
-      body: SingleChildScrollView(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Container(
-            height: 500,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                  (widget.img.toString().substring(0,  widget.img.toString().length-5) + "1.jpg").toString(),
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.black87,
-                    Colors.transparent,
-                  ],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          //automaticallyImplyLeading: false,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        backgroundColor: Colors.black87,
+        extendBodyBehindAppBar: true,
+        body: SingleChildScrollView(
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Container(
+              height: 500,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    (widget.img.toString().substring(0,  widget.img.toString().length-5) + "1.jpg").toString(),
+                  ),
+                  fit: BoxFit.cover,
                 ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 15, bottom: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.response["data"][widget.champ]["name"],
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 5),
-                    const SizedBox(height: 5),
-                    Row(
-                      children: [
-                        Text(
-                          widget.response["data"][widget.champ]["title"],
-                          style: const TextStyle(
-                            color: Colors.yellow,
-                            fontSize: 17,
-                          ),
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black87,
+                      Colors.transparent,
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15, bottom: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.response["data"][widget.champ]["name"],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
                         ),
-                        const SizedBox(width: 5),
-                      ],
-                    )
-                  ],
+                      ),
+                      const SizedBox(height: 5),
+                      const SizedBox(height: 5),
+                      Row(
+                        children: [
+                          Text(
+                            widget.response["data"][widget.champ]["title"],
+                            style: const TextStyle(
+                              color: Colors.yellow,
+                              fontSize: 17,
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 15),
-            child: Text(
-              "Overview",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            const Padding(
+              padding: EdgeInsets.only(left: 15),
+              child: Text(
+                "Overview",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
-            child: Text(
-              widget.response["data"][widget.champ]["blurb"],
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 15,
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
+              child: Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blueGrey.shade900,
+                ),
+                child: Text(
+                  widget.response["data"][widget.champ]["blurb"],
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
               ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
